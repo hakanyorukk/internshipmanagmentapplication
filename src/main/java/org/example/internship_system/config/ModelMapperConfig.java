d537a84ba36ddd1209ecc4eb14e4713149f35d31
hakanyorukk <hakanyorukomer@hotmail.com>
2026-07-02 12:13:29 +0300

    feat: add User, StudentProfile, Application repositories

--- src/main/java/org/example/internship_system/config/ModelMapperConfig.java
+++ src/main/java/org/example/internship_system/config/ModelMapperConfig.java
@@ -1,4 +1,12 @@
 package org.example.internship_system.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
+
+   @Bean
+    public ModelMapper modelMapper(){return new ModelMapper();}
 }
